764eb411eae267ed99c1cc584f046d7cca71d2b8
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-20 23:49:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,76 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0) {
+        resolve('First promise was resolved');
+      }
+    },
+    { once: true },
+  );
+
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('First promise was rejected');
+  }, 3000);
+});
+
+firstPromise.then(successMessage).catch(errorMessage);
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      if (e.button === 0 || e.button === 2) {
+        resolve('Second promise was resolved');
+      }
+    },
+    { once: true },
+  );
+});
+
+secondPromise.then(successMessage).catch(errorMessage);
+
+let leftClick = false;
+let rightClick = false;
+
+const thirdPromise = new Promise((resolve, reject) => {
+  function click(e) {
+    if (e.button === 0) {
+      leftClick = true;
+    }
+
+    if (e.button === 2) {
+      rightClick = true;
+    }
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+      document.removeEventListener('mousedown', click);
+    }
+  }
+
+  document.addEventListener('mousedown', click);
+});
+
+thirdPromise.then(successMessage).catch(errorMessage);
+
+function successMessage(message) {
+  const success = document.createElement('div');
+
+  success.dataset.qa = 'notification';
+  success.className = 'success';
+  success.textContent = message;
+  document.body.appendChild(success);
+}
+
+function errorMessage(message) {
+  const error = document.createElement('div');
+
+  error.dataset.qa = 'notification';
+  error.className = 'error';
+  error.textContent = message;
+  document.body.appendChild(error);
+}
